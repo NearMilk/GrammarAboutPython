5d41c09da8b4a7388025e83445cf9cb72b40bb9f
nearMilk <yzy153063616@qq.com>
2017-08-21 00:29:09 +0800

    My Test

---
+++ day1/Variable.py
@@ -0,0 +1,10 @@
+name = "NearMilk"
+
+print("My name is",name, ", is a fool man")
+
+name2 = name
+
+print("Name2 = ",name2,",Name1 = ",name)
+name = "Near"
+
+print("Name2 = ",name2,",Name1 = ",name)
